bc389f35a4ffad5ba62d198a3ab96db85f0fdaa3
Alejando del Rio (adelriosantiago) <adelriosantiago@gmail.com>
2015-08-23 04:19:49 -0600

    JSLint fixes

--- io.js
+++ io.js
@@ -5,65 +5,80 @@ var boardSize = 32;
 
 //Create the board
 function matrix(rows, cols) {
-    var arr = [];
+    'use strict';
+    var arr = [], i, j;
+
+    //var i, j;
     //Creates all lines:
-    for (var i = 0; i < rows; i++) {
+    for (i = 0; i < rows; i += 1) {
         //Creates an empty line
         arr.push([]);
         //Adds cols to the empty line:
         arr[i].push(new Array(cols));
-        for (var j = 0; j < cols; j++) {
+        for (j = 0; j < cols; j += 1) {
             //Initial value
             arr[i][j] = 0;
         }
     }
     return arr;
 }
-var randNum = function() {
+var randNum = function () {
+    'use strict';
     return Math.floor(Math.random() * 2);
-}
+};
 
 var board = matrix(boardSize, boardSize);
 var next = matrix(boardSize, boardSize);
 function updateBoard() {
+    'use strict';
+
+    var neighbors = 0, x, y, i, j, temp;
+
     //Loop through every spot in our 2D array and check spots neighbors
-    for (var x = 1; x < boardSize - 1; x++) {
-        for (var y = 1; y < boardSize - 1; y++) {
+    for (x = 1; x < boardSize - 1; x += 1) {
+        for (y = 1; y < boardSize - 1; y += 1) {
             //Add up all the states in a 3 * 3 surrounding grid
-            var neighbors = 0;
-            for (var i = -1; i <= 1; i++) {
-                for (var j = -1; j <= 1; j++) {
+            for (i = -1; i <= 1; i += 1) {
+                for (j = -1; j <= 1; j += 1) {
                     neighbors += board[x + i][y + j];
                 }
             }
             neighbors -= board[x][y]; //Substract the current cell since it was added
 
             //Rules of Life
-            if ((board[x][y] == 1) && (neighbors < 2)) next[x][y] = 0; //Loneliness
-            else if ((board[x][y] == 1) && (neighbors > 3)) next[x][y] = 0; //Overpopulation
-            else if ((board[x][y] == 0) && (neighbors == 3)) next[x][y] = 1; //Reproduction
-            else next[x][y] = board[x][y]; //Stasis
+            if ((board[x][y] === 1) && (neighbors < 2)) {
+                next[x][y] = 0; //Loneliness
+            } else if ((board[x][y] === 1) && (neighbors > 3)) {
+                next[x][y] = 0; //Overpopulation
+            } else if ((board[x][y] === 0) && (neighbors === 3)) {
+                next[x][y] = 1; //Reproduction
+            } else {
+                next[x][y] = board[x][y]; //Stasis
+            }
         }
     }
-    var temp = board;
+    temp = board;
     board = next;
     next = temp;
 }
 
 function sendUpdate() {
-  setTimeout(function() {
-    //Do something here
-    updateBoard();
-    io.emit('board update', board);
-    console.log('Doing a request');
-    sendUpdate();
-  }, 500);
+    'use strict';
+
+    setTimeout(function () {
+        //Do something here
+        updateBoard();
+        io.emit('board update', board);
+        console.log('Doing a request');
+        sendUpdate();
+    }, 500);
 }
 sendUpdate(5000);
 
 io.on('connection', function (socket) {
+    'use strict';
     console.log('connection');
-    socket.on('recreate', function() {
+    socket.on('recreate', function () {
         //Can be DRY'fyed
         board = matrix(boardSize, boardSize);
         next = matrix(boardSize, boardSize);
@@ -74,18 +89,20 @@ io.on('connection', function (socket) {
         io.emit('board update', board);
     });*/
     
-    socket.on('draw', function(data) {
+    socket.on('draw', function (data) {
+        var x, y, offset, coordX, coordY;
+
         console.log(data);
         
         //Assume dimensions are correct
-        var offset = Math.floor(data.pattern.length / 2);
-        for (var x = 0; x < data.pattern.length; x++) {
-            for (var y = 0; y < data.pattern[x].length; y++) {
-                if (data.pattern[x][y] == 1) {
-                    var coordX = parseInt(data.x) + x - offset;
-                    var coordY = parseInt(data.y) + y - offset;
+        offset = Math.floor(data.pattern.length / 2);
+        for (x = 0; x < data.pattern.length; x += 1) {
+            for (y = 0; y < data.pattern[x].length; y += 1) {
+                if (data.pattern[x][y] === 1) {
+                    coordX = parseInt(data.x, 10) + x - offset;
+                    coordY = parseInt(data.y, 10) + y - offset;
                     if ((coordX > 0) && (coordX < boardSize) && (coordY > 0) && (coordY < boardSize)) {
-                        board[coordX][coordY] = 1;    
+                        board[coordX][coordY] = 1;
                     }
                     io.emit('board update', board);
                 }
