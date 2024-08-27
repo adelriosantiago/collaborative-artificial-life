863704f030359ed75961059b162de62c8e2f3e6d
Alejandro del Rio Santiago (@adelriosantiago) <adelriosantiago@gmail.com>
2024-08-27 15:11:35 -0600

    Add correct url

--- public/javascripts/asciiautomata.js
+++ public/javascripts/asciiautomata.js
@@ -16,9 +16,9 @@ $(document).ready(function () {
   //Real-time user interface
   var test_box,
     mouseTimer = null,
-    //socket = io.connect('http://alife.adelriosantiago.com'), //Test on the real server with reverse proxy
+    socket = io.connect("http://automata.adelriosantiago.com"), //Test on the real server with reverse proxy
     //socket = io.connect('adelriosantiago.com:7000'), //Test on the real server via the port number
-    socket = io.connect("localhost:7000"), //Test on localhost
+    //socket = io.connect("localhost:7000"), //Test on localhost
     patternSize = 9,
     pattern
 
